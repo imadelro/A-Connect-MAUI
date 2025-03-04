09b975e0db625124f23942088b7026d1a7675a79
Malena del Rosario <malena.delrosario@student.ateneo.edu>
2025-03-04 20:47:50 +0800

    UI for login page

--- A-Connect/AppShell.xaml.cs
+++ A-Connect/AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace A_Connect;
+﻿using A_Connect.Views;  // Add this
+
+namespace A_Connect;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public AppShell()
+    {
+        InitializeComponent();
+
+        // Fix: Remove nameof(LoginPage) and use string
+        Routing.RegisterRoute("LoginPage", typeof(LoginPage));
+    }
 }
