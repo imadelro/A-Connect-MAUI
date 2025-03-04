e113711490dd4e3f2dc7addb52170e3964bfc67e
Malena del Rosario <malena.delrosario@student.ateneo.edu>
2025-03-04 21:47:57 +0800

    add startpage xaml and xaml.cs

---
+++ A-Connect/Views/StartPage.xaml.cs
@@ -0,0 +1,23 @@
+using Microsoft.Maui.Controls;
+using System;
+
+namespace A_Connect.Views
+{
+    public partial class StartPage : ContentPage  // Must be partial
+    {
+        public StartPage()
+        {
+     
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("LoginPage");
+        }
+
+        private async void OnRegisterClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("RegisterPage");
+        }
+    }
+}
