43cb4ff7dec73b99933fc9b3e379fe80ab2859de
nmxnii <nicole.manibog@student.ateneo.edu>
2025-04-25 01:29:52 +0800

    added navbar page

--- A-Connect/Views/HomePage.xaml.cs
+++ A-Connect/Views/HomePage.xaml.cs
@@ -1,15 +1,21 @@
 using Microsoft.Maui.Controls;
+using A_Connect.ViewModels;
+using System.Windows.Input;
 
 namespace A_Connect.Views
 {
     public partial class Homepage : ContentPage
     {
+
         public Homepage()
         {
             InitializeComponent();
             string username = App.CurrentUser?.Username ?? "Guest";
             WelcomeLabel.Text = $"Welcome, {username}!";
+            BindingContext = new HomepageViewModel();
         }
+
+
 
         private async void OnScheduleTradingClicked(object sender, EventArgs e)
         {
@@ -18,24 +24,29 @@ namespace A_Connect.Views
 
         private async void OnProfsToPickClicked(object sender, EventArgs e)
         {
-            // Navigate to the Profs to Pick News Feed page
-            // Make sure the route "ProfsToPickNewsFeedPage" is registered in AppShell.
             await Shell.Current.GoToAsync("ProfsToPickNewsFeedPage");
         }
+
         private async void OnTutorFinderClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("TutorFinderPage");
+        }
 
-        } 
         private async void OnInternNJobsClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("InternNJobsNewsfeedPage");
-        }          
+        }
 
         private async void OnMarketplaceClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync(nameof(MarketplaceNewsFeedPage));
+            await Shell.Current.GoToAsync("MarketplaceNewsFeedPage");
         }
+        private async void OnNavIconClicked(object sender, EventArgs e)
+{
+    // Toggle visibility
+    Menu_container.IsVisible = !Menu_container.IsVisible;
+    await Menu_container.TranslateTo(0, 0, 250, Easing.CubicOut);
+}
 
         private async void OnLogoutClicked(object sender, EventArgs e)
         {
@@ -43,14 +54,9 @@ namespace A_Connect.Views
 
             if (confirm)
             {
-                // Clear user session data
                 App.CurrentUser = null;
-
-                // Remove any stored credentials or tokens
                 Preferences.Remove("Username");
                 Preferences.Remove("IsLoggedIn");
-
-                // Navigate back to login page
                 await Shell.Current.GoToAsync("StartPage");
             }
         }
